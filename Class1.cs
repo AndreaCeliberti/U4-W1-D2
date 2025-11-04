cb1e1cedb0f8e64c415e48afe428f823cb05910f
Andrea Celiberti <andrea.celiberti2@gmail.com>
2025-11-04 16:26:55 +0100

    Aggiungere i file di progetto.

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U4_W1_D2
+{
+    public class Persona 
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        public Persona (string name, string lastname, int age)
+        {
+            Name = name;
+            Lastname = lastname; 
+            Age = age;  
+
+           
+    }
+        public string getName() { return Name; }
+        public string getLastname() { return Lastname; }
+        public int getAge() { return Age; }
+
+       /*public void getDetail() { return Name, return Lastname, return Age}*/
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+using U4_W1_D2;
+
+var persona1 = new Persona("Andrea", "Celiberti", 38);
+
+
+Console.WriteLine(persona1.getName());
+
+Console.WriteLine(persona1.getLastname());
+
+Console.WriteLine(persona1.getAge());
+
